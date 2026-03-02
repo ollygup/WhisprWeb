b0bcd0be7e5302b4e13b5af1e44c1431e052e0b0
Ollygup <82217636+ollygup@users.noreply.github.com>
2026-03-02 18:53:27 +0800

    remove console log from prod

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { enableProdMode } from '@angular/core';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+  console.log = () => {};
+}
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
